2a5e636b12bb75e39d35ae8a5050b42a904ca2fb
Leo <danielesquivel00.lded@gmail.com>
2022-11-28 23:24:32 -0600

    Ajusten en la configuracion del servidor

--- models/server.js
+++ models/server.js
@@ -1,36 +1,64 @@
 const express = require('express');
 const cors    = require('cors');
 
+const { dbConnection } = require('../database/config.database');
+
 class Server {
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        
-        this.paths = {
 
+        this.paths = {
+            auth:       '/api/auth',
+            categorias: '/api/categorias',
+            productos: '/api/productos',
+            usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads'
         }
 
 
+        //Conectar a base datos
+        this.conectarDB()
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas
+        this.routes();
     }
 
     async conectarDB() {
-
+        await dbConnection();
     }
 
     middlewares() {
+        
+        //CORS
+        this.app.use( cors() );
 
+        //Parse body
+        this.app.use( express.json() );
+
+        //Directorio publico
+        // this.app.use( express.static('public') );
     }
 
     routes() {
 
+        // this.app.use(this.paths.auth, require('../routes/auth.routes'));
+        // this.app.use(this.paths.categorias, require('../routes/categorias.routes'));
+        // this.app.use(this.paths.productos, require('../routes/productos.routes'));
+        // this.app.use(this.paths.usuarios, require('../routes/user.routes'));
+        // this.app.use(this.paths.uploads, require('../routes/uploads.routes'));
+
     }
 
     listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en el puerto: ${this.port}`);
-        })
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto:', this.port);
+        });
     }
 }
 
-module.exports = Server
+module.exports = Server;
